42325b06260fd0f8366c5248830c1ed14d343eb6
igorbock <igorbdaluz@hotmail.com>
2025-02-22 19:17:01 -0300

    Primeiro commit

---
+++ FingerprintWF/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace FingerprintWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StatusText = new System.Windows.Forms.Label();
+            this.image = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StatusText
+            // 
+            this.StatusText.AutoSize = true;
+            this.StatusText.Location = new System.Drawing.Point(12, 438);
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(35, 13);
+            this.StatusText.TabIndex = 0;
+            this.StatusText.Text = "label1";
+            // 
+            // image
+            // 
+            this.image.Location = new System.Drawing.Point(12, 12);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(465, 413);
+            this.image.TabIndex = 1;
+            this.image.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(402, 434);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 469);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.image);
+            this.Controls.Add(this.StatusText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label StatusText;
+        private System.Windows.Forms.PictureBox image;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ FingerprintWF/Form1.cs
@@ -0,0 +1,99 @@
+﻿using DPFP;
+using DPFP.Capture;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FingerprintWF
+{
+    public partial class Form1 : Form, DPFP.Capture.EventHandler
+    {
+        private DPFP.Capture.Capture _capturer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                _capturer = new Capture();
+                if (_capturer != null)
+                {
+                    _capturer.EventHandler = this;
+                    MakeReport("Pronto para iniciar");
+                }
+                else
+                    MakeReport("ainda não está pronto");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        protected void MakeReport(string message)
+        {
+            Invoke(new Action(delegate ()
+            {
+                StatusText.Text = message;
+            }));
+        }
+
+        public void OnComplete(object Capture, string ReaderSerialNumber, Sample Sample)
+        {
+            MakeReport("A captura foi realizada");
+            Proccess(Sample);
+        }
+
+        public void OnFingerGone(object Capture, string ReaderSerialNumber) => MakeReport("O dedo foi removido do leitor");
+
+        public void OnFingerTouch(object Capture, string ReaderSerialNumber) => MakeReport("O leitor foi tocado");
+
+        public void OnReaderConnect(object Capture, string ReaderSerialNumber) => MakeReport("Conectada");
+
+        public void OnReaderDisconnect(object Capture, string ReaderSerialNumber) => MakeReport("Desconectada");
+
+        public void OnSampleQuality(object Capture, string ReaderSerialNumber, CaptureFeedback CaptureFeedback)
+        {
+            if (CaptureFeedback == CaptureFeedback.Good)
+                MakeReport("A captura está boa");
+            else
+                MakeReport("A captura está ruim");
+        }
+
+        protected virtual void Proccess(DPFP.Sample sample)
+        {
+            DrawImage(ConvertSampleToBitmap(sample));
+        }
+
+        protected Bitmap ConvertSampleToBitmap(DPFP.Sample sample)
+        {
+            DPFP.Capture.SampleConversion conversor = new SampleConversion();
+            Bitmap bitmap = null;
+            conversor.ConvertToPicture(sample, ref bitmap);
+
+            return bitmap;
+        }
+
+        private void DrawImage(Bitmap bitmap) => image.Image = new Bitmap(bitmap, image.Size);
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (_capturer != null)
+            {
+                try
+                {
+                    _capturer.StartCapture();
+                    MakeReport("Faça a leitura do dedo");
+                }
+                catch (Exception ex)
+                {
+                    MakeReport($"um erro aconteceu: {ex.Message}");
+                }
+            }
+        }
+    }
+}
